db7ac6e6b0ef5f98892ccc1b9ff979202b7a83ed
N7Tristan <tristan.schultz.57@gmail.com>
2015-08-29 23:58:59 +0800

    scrolling à terminer

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 
+// **************************************************************************************
+
+int resWidth;
+int resHeight;
+
+// ************************************************************************************** 
+
 
 void keyboardEvents()
 {
@@ -18,16 +25,23 @@ void keyboardEvents()
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1920,1080),"TestWindow");
+
+	std::cout<<"Resolution horizontale : ";
+	std::cin>>resWidth;
+	std::cout<<"Resolution verticale : ";
+	std::cin>>resHeight;
+
+
+	sf::RenderWindow window(sf::VideoMode(resWidth, resHeight),"TestWindow");
 
 	sf::Clock clock;
 	sf::Time millis = clock.getElapsedTime();
 	sf::Time rotationTemp = clock.getElapsedTime();
 
-	sf::View view(sf::Vector2f(960, 540), sf::Vector2f(1920, 1080));
+	sf::View view(sf::Vector2f(resWidth/2, resHeight/2), sf::Vector2f(resWidth, resHeight));
 	window.setView(view);
 
-	view.setCenter(960, 540);
+	view.setCenter(resWidth/2, resHeight/2);
 
 	sf::Texture backTexture;
 	if (!backTexture.loadFromFile("img/back2.png"))
@@ -46,12 +60,12 @@ int main()
  	sf::Sprite backSprite;
  	sf::Sprite catSprite;
 
- 	backSprite.setTextureRect(sf::IntRect(0, 0, 9600, 1080));
+ 	backSprite.setTextureRect(sf::IntRect(0, 0, 5*resWidth, resHeight));
 
  	backSprite.setTexture(backTexture);
  	catSprite.setTexture(catTexture);
 
- 	catSprite.move(960,540);
+ 	catSprite.move(resWidth/2, resHeight/2);
  	catSprite.setOrigin(50,64);
 
  	unsigned int angle=0;
@@ -68,10 +82,6 @@ int main()
         }
 
 		
-
- 
-
-
 		window.clear();
 		window.draw(backSprite);
 		window.draw(catSprite);
@@ -79,41 +89,54 @@ int main()
 
 		millis = clock.getElapsedTime();
 
-		
 
 		if (millis.asMilliseconds()>=1)
 		{
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 			{
-				catSprite.move(-10,0);
-				view.move(-10, 0);
-				window.setView(view);
+				if(catSprite.getPosition().x>0)
+					catSprite.move(-10,0);
+				
+				if(catSprite.getPosition().x>960)
+					view.move(-10, 0);
+				
 			}
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 			{
-				catSprite.move(10,0);
-				view.move(10, 0);
-				window.setView(view);
+				if(catSprite.getPosition().x<5*resWidth)
+					catSprite.move(10,0);
+
+				if(catSprite.getPosition().x<8640)
+					view.move(10, 0);
+				
 		}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			{	catSprite.move(0,-10);
-				view.move(0, -10);
-				window.setView(view);
+			{	
+				if(catSprite.getPosition().y>0)
+					catSprite.move(0,-10);
+				
+				if(catSprite.getPosition().y<540)
+					view.move(0, -10);
+				
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 			{
-				catSprite.move(0,10);
-				view.move(0, 10);
-				window.setView(view);
+				if(catSprite.getPosition().y<resHeight)
+					catSprite.move(0,10);
+				
+				if(catSprite.getPosition().y>540)
+				{
+					view.move(0, 10);
+				}
+				
 			}
+
+			window.setView(view);
+
+
 		}
 
-		rotationTemp = clock.getElapsedTime();
-		if (rotationTemp.asMilliseconds()>=50)
-		{
-			catSprite.rotate(sqrt(angle++));
-			clock.restart();
-		}
+		clock.restart();
 	}
 }
